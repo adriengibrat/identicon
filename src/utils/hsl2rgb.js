2866a17f775216a15332c980433f549e09a77169
Adrien Gibrat <adrien.gibrat@gmail.com>
2015-12-09 09:25:15 +0100

    wip on hls2rgb

--- src/utils/hsl2rgb.js
+++ src/utils/hsl2rgb.js
@@ -1,6 +1,7 @@
-export default (hue, saturation = .5, light = .7) => {
+/*
+export default function hls2rgb (hue, saturation = .5, light = .7) {
 	hue *= 6
-	let chanel = [
+	let chanels = [
 		light += saturation *= light < .5 ? light : 1 - light,
 		light - hue % 1 * saturation * 2,
 		light -= saturation *= 2,
@@ -9,5 +10,28 @@ export default (hue, saturation = .5, light = .7) => {
 		light + saturation
 	]
 
-	return [~~hue, hue | 16, hue | 8].map(x => chanel[x % chanel.length])
+	return [~~hue, hue | 16, hue | 8].map(x => chanels[x % chanels.length])
 }
+*/
+const HUE_FACTOR = 360 / 60
+
+// credits: https://en.wikipedia.org/wiki/HSL_and_HSV, https://gist.github.com/aemkei/1325937
+export default function hls2rgb (hue, saturation = .5, light = .7) {
+	hue *= HUE_FACTOR
+	let chroma = (1 - Math.abs(2 * light - 1)) * saturation,
+		component = chroma * (1 - Math.abs(hue % 2 - 1)),
+		lightness = light - chroma / 2,
+		chanels = [chroma + lightness, component + lightness, lightness],
+		length = channels.length
+
+	//return [hue | 8, hue | 16, Math.floor(hue)].map(index => chanels[index % chanels.length])
+	return [chanels[(hue | 8) % length], chanels[(hue | 16) % length], chanels[Math.floor(hue) % length]]
+}
+
+hls2rgb.range = (range) => function (/*hue, saturation, light*/) {
+	//return hls2rgb(hue, saturation, light).map((chanel) => Math.round(chanel * range))
+	let [red, green, blue] = hls2rgb.apply(null, arguments)
+	return [Math.round(red * range), Math.round(green * range), Math.round(blue * range)]
+}
+
+hls2rgb.depth = (depth) => hls2rgb.range(Math.pow(depth, 2) - 1)
